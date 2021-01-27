ca7cb632058270a2a76b6a8ae12d6f0886e9a4a9
Ekaterina Parfenteva <katerinaprf27@gmail.com>
2021-01-27 16:05:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_00;
+        int remittance = 500_000_000;
+        int total = balance + remittance;
+        System.out.println(total);
+    }
+}
